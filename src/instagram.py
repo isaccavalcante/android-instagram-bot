509f4603bf03fc71330933f1be6d1f2f10016a42
Isac Cavalcante <isaccavalcante@alu.ufc.br>
2020-02-20 19:20:30 -0300

    Fix unfollow loop

--- src/instagram.py
+++ src/instagram.py
@@ -11,8 +11,8 @@ def type_word(word):
 
 def dump():
     xml = d.dump("screen.xml")
-    with open("screen .csv", 'w') as f:
-        f.write("text,content-desc" + "\n")
+    with open("screen.csv", 'w') as f:
+        f.write("text,content-desc,id" + "\n")
         soup = BeautifulSoup(xml, "html.parser")
         for node in soup.find_all("node"):
             line = f"{node['text']},{node['content-desc']},{node['resource-id']}"
@@ -40,20 +40,35 @@ def follow_commentators():
 
 
 def pretend_normal_usage():
+    dump()
+    d(description="Home", className="android.widget.FrameLayout").click()
+    #d.press.back()
+    d(descriptionContains="'s story at column")[1].drag.to(descriptionContains="'s story at column")
+    d.press.back()
+    d(descriptionContains="'s story at column")[1].click()
+    time.sleep(30)
+    d.press.back()
+    #d(description="Image").swipe.up(steps=10)
+    #time.sleep(5)
     d(description="Profile").click()
-    d(description="Search and Explore").click()
-    d(description="Profile").click()
-    d(text="Following").click()
+    #d(text="Following").click()
 
 def unfollow_all():
     enter_instagram()
     d(description="Profile").click()
+    d(description="Profile").click()
     d(text="Following").click()
-    for i in range(100):
+    count = 0
+    while True:
         d(text="Following").click()
         if d(text="Unfollow").exists:
             d(text="Unfollow").click()
-        print(f"Unfollowed {i + 1} accounts")
+        print(f"Unfollowed {count + 1} accounts")
+        count += 1
         d(text="Follow")[-1].drag.to(textContains="Followers")
-        time.sleep(30)
+        pretend_normal_usage()
+        #time.sleep(30)
 
+#unfollow_all()
+#pretend_normal_usage()
+#pretend_normal_usage()
